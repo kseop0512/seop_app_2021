ec024cdee4b38f2f862ef217c93fcdbfb6813f89
kks0512 <k_seop0512@hanmail.net>
2021-10-15 18:38:37 +0900

    api 적용

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
 import React from "react";
 //import PropTypes from "prop-types";
 import axios from "axios";
-
+import Movie from "./Movie";
+import "./App.css";
+  
 
 // class component
 class App extends React.Component{
 
-    state = {
-        isLoading: true,
-        movies: []
-    }
+	state = {
+		isLoading: true,
+		movies: []
+	}
 
-    getMovies = async () => {
+	getMovies = async () => {
 
-        const {
-            data: {
-                data: { movies }
-            }
-        } = await axios.get("https://yts-proxy.now.sh/list_movies.json")
-        
-        this.setState({ movies, isLoading: false })
-    }
+		const {
+			data: {
+				data: { movies }
+			}
+		} = await axios.get(
+			"https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+		);
+		//console.log(movies)
+		this.setState({ movies, isLoading: false })
+	} 
 
-    componentDidMount() {
+	componentDidMount() {
 
-        this.getMovies();
-         
-    }
-    render() {
+		this.getMovies();   
+				
+	}
+	render() {
 
-        const { isLoading } = this.state;
-        return <div>{isLoading ? "Loading..." : "We are ready"}</div>
-        
-    }
+		const { isLoading, movies } = this.state;
+		return (
+			<section className="container">
+				{isLoading 
+					? (
+					<div className="loader">
+						<span className="loader__text">Loading...</span>    
+					</div> 
+					) 
+					: (
+						<div className="moives">
+							{movies.map(movie => (
+								<Movie
+									key={movie.id}
+									id={movie.id}
+									year={movie.year}
+									title={movie.title}
+									summary={movie.summary}
+									poster={movie.medium_cover_image}
+									genres={movie.genres}
+								/>
+							))}
+						</div>
+					)
+				}
+			</section>
+		);
+	}
 }
 
 
